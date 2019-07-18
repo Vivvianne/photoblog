8677ceae1af928c598bae661b5d1a5e126d00d0d
Vivvianne Kimani <ndiahav@gmail.com>
2019-07-18 12:01:11 +0300

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nkimani/Documents/Moringa-core/Python/django/photoblog/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
